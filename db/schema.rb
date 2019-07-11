cbd137a6876102cce152e87ed3707481d634dc07
Arye Eidelman <aryedov.e@gmail.com>
2019-07-11 03:33:27 -0400

    Change game state to array

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20160212160805) do
 
   create_table "games", force: :cascade do |t|
-    t.text     "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "state",      default: "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]"
+    t.datetime "created_at",                                                                    null: false
+    t.datetime "updated_at",                                                                    null: false
   end
 
 end
